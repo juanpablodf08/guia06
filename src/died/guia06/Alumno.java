95502ad7611d89ce34de19a377e59e4672251d6f
juanpablodf08 <jpdefrutos@gmail.com>
2020-04-16 11:28:01 -0300

    Proyecto descargado. Copiando los metodos

--- src/died/guia06/Alumno.java
+++ src/died/guia06/Alumno.java
@@ -3,13 +3,114 @@ package died.guia06;
 import java.util.List;
 
 
-public class Alumno {
+public class Alumno implements Comparable<Alumno>{
 
 	private String nombre;
 	private Integer nroLibreta;
 	private List<Curso> cursando;
 	private List<Curso> aprobados;
 
+	public Alumno(String nombre, Integer nroLibreta, List<Curso> cursando, List<Curso> aprobados) {
+		super();
+		this.nombre = nombre;
+		this.nroLibreta = nroLibreta;
+		this.cursando = cursando;
+		this.aprobados = aprobados;
+	}
+	
+	//DOS ALUMNOS SON IGUALES SI TIENEN MISMO NUMERO DE LIBRETA
+	public boolean equals(Object obj) {
+			
+			Alumno other = (Alumno) obj;
+			if (nroLibreta == null) {
+				if (other.nroLibreta != null)
+					return false;
+			} else if (!nroLibreta.equals(other.nroLibreta))
+				return false;
+			return true;
+	}
+		
+	//Cuando aprueba un curso, se quita el curso de la lista "cursando" y se agrega a la lista de cursos aprobados
+	public boolean aproboCurso (Curso unCurso) {
+		
+			for(int i = 0; i < cursando.size();i++) {
+				if(cursando.get(i).getId() == unCurso.getId()) {  //SI EL CURSO QUE PASE ESTA EN LA LISTA DE "CURSANDO"
+					
+					cursando.remove(i);
+					aprobados.add(unCurso);
+					System.out.println("Aprobo el curso "+unCurso.getNombre());
+					return true;										//retorna 1 si estaba cursando y aprobo
+				}
+				System.out.println("No esta cursando dicho curso");
+			}														//si no estaba cursando, retorna cero, es decir que no aprobo ese curso
+			return false;
+	}	
+	
+	public void empezoAcursar(Curso unCurso) {
+		System.out.println("Se inscribio al curso "+unCurso.getNombre());
+		cursando.add(unCurso);
+	}
+	
+	public int compareTo(Alumno a) {
+		if( nombre.compareTo(a.getNombre()) > 0) { 		//cadena1 va después que cadena2
+			return 1;
+		}else if (nombre.compareTo(a.getNombre()) < 0)   //"cadena1 va antes que cadena2"
+					return -1;
+			  else return 0;										//cadenas iguales
+    }
+	
+	public Integer creditosQuePoseeAlumno() {
+		Integer credTotales=0;
+		
+		if(aprobados != null) {
+			for(Curso unCurso : aprobados) {
+				credTotales = credTotales + unCurso.getCreditos();
+			}
+		}
+		return  credTotales;
+	}
+	
+	public int getCantidadCursando() {
+		
+		if(cursando == null) {
+			return 0;
+		}else return cursando.size();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getNroLibreta() {
+		return nroLibreta;
+	}
+
+	public void setNroLibreta(Integer nroLibreta) {
+		this.nroLibreta = nroLibreta;
+	}
+
+	public List<Curso> getCursando() {
+		return cursando;
+	}
+
+	public void setCursando(List<Curso> cursando) {
+		this.cursando = cursando;
+	}
+
+	public List<Curso> getAprobados() {
+		return aprobados;
+	}
+
+	public void setAprobados(List<Curso> aprobados) {
+		this.aprobados = aprobados;
+	}
+	
+	
+	
 	public int creditosObtenidos() {
 		return 1;
 	}
--- src/died/guia06/Curso.java
+++ src/died/guia06/Curso.java
@@ -1,6 +1,9 @@
 package died.guia06;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import died.guia06.util.Registro;
@@ -31,6 +34,18 @@ public class Curso {
 		this.log = new Registro();
 	}
 	
+	public Curso(Integer id, String nombre, Integer cicloLectivo, Integer cupo, List<Alumno> inscriptos,
+			Integer creditos, Integer creditosRequeridos, Registro log) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cicloLectivo = cicloLectivo;
+		this.cupo = cupo;
+		this.inscriptos = inscriptos;
+		this.creditos = creditos;
+		this.creditosRequeridos = creditosRequeridos;
+		this.log = log;
+	}
 
 	/**
 	 * Este método, verifica si el alumno se puede inscribir y si es así lo agrega al curso,
@@ -44,19 +59,149 @@ public class Curso {
 	 *      c) puede estar inscripto en simultáneo a no más de 3 cursos del mismo ciclo lectivo.
 	 * @param a
 	 * @return
+	 * @throws IOException 
 	 */
-	public Boolean inscribir(Alumno a) {
-		log.registrar(this, "inscribir ",a.toString());
-		return false;
-	}
 	
+	public Boolean inscribir(Alumno a) throws ArgumentInvalidException, IOException{
+		
+		if(a.creditosQuePoseeAlumno() >= creditosRequeridos) {
+			//PODRIA USAR TB: throw new ArgumentInvalidException("No tiene los cr�ditos necesarios")
+			log.registrar(this, "inscribir ",a.toString());
+			return false; 												//RETORNO FALSE PORQUE NO PUEDO SALVAR  LA VALIDACION,
+																		//SI NO TIENE CREDITOS, NO PUEDE
+		}
+		if(inscriptos.size() < cupo) {
+			log.registrar(this, "inscribir ",a.toString());
+			throw new ArgumentInvalidException("Cupo del curso");    //LANZO EXCEPTION XQ PUEDO SALVAR  LA VALIDACION; 
+		}															//POR EJ PROBANDO EN OTRA COMISION
+		if(a.getCantidadCursando() < 3) {
+			log.registrar(this, "inscribir ",a.toString());
+			throw new ArgumentInvalidException("Cant de materias cursando mayor a 3"); //LANZO EXCEPTION XQ PUEDO SALVAR  LA VALIDACION
+		}																				//POR EJ RENUNCIAN A UNA MATERIA
+		
+		inscriptos.add(a);
+		Curso newCurso = new Curso();
+		
+		newCurso.setId(this.id);
+		newCurso.setNombre(this.nombre);
+		newCurso.setCicloLectivo(this.cicloLectivo);
+		newCurso.setCupo(this.cupo);
+		newCurso.setInscriptos(this.inscriptos);
+		newCurso.setCreditos(this.creditos);
+		newCurso.setCreditosRequeridos(this.creditosRequeridos);
+		newCurso.setLog(this.log);
+
+		System.out.println("Se inscribio al alumno: "+a.getNombre());
+		a.empezoAcursar(newCurso);
+		log.registrar(this, "inscribir ",a.toString());
+		return true;
+		
+	}	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCicloLectivo() {
+		return cicloLectivo;
+	}
+
+	public void setCicloLectivo(Integer cicloLectivo) {
+		this.cicloLectivo = cicloLectivo;
+	}
+
+	public Integer getCupo() {
+		return cupo;
+	}
+
+	public void setCupo(Integer cupo) {
+		this.cupo = cupo;
+	}
+
+	public List<Alumno> getInscriptos() {
+		return inscriptos;
+	}
+
+	public void setInscriptos(List<Alumno> inscriptos) {
+		this.inscriptos = inscriptos;
+	}
+
+	public Integer getCreditos() {
+		return creditos;
+	}
+
+	public void setCreditos(Integer creditos) {
+		this.creditos = creditos;
+	}
+
+	public Integer getCreditosRequeridos() {
+		return creditosRequeridos;
+	}
+
+	public void setCreditosRequeridos(Integer creditosRequeridos) {
+		this.creditosRequeridos = creditosRequeridos;
+	}
+
+	public Registro getLog() {
+		return log;
+	}
+
+	public void setLog(Registro log) {
+		this.log = log;
+	}
+
 	
 	/**
 	 * imprime los inscriptos en orden alfabetico
 	 */
-	public void imprimirInscriptos() {
-		log.registrar(this, "imprimir listado",this.inscriptos.size()+ " registros ");
+	
+	public void imprimirInscriptos() throws Exception {
+		
+		try {
+			System.out.println("ESTOY EN IMPRIMIR INSCRIPTOS");
+			System.out.println(inscriptos);
+			Collections.sort((List<Alumno> inscriptos);  //ORDENA LA LISTA SEGUN EL CRITERIO Q DEFINI EN "CompareTo", en este caso num de Libreta
+			System.out.println(inscriptos);
+			
+			//Comparar por la interface Comparator por nombre
+			
+			CompararNombre comparador = new CompararNombre();				
+			Collections.sort(inscriptos, comparador);
+			System.out.println(inscriptos);
+			
+			//OTRA FORMA, para no tener q andar creando clases para cada criterio, es difiniendo directamente en el metodo
+			//comparo por creditos
+			Collections.sort(								// aca comparo por cantidad de creditos de cada alumno
+							inscriptos, 
+							new Comparator<Alumno>() {
+								public int compare(Alumno a1, Alumno a2) {
+									int creditos1 = a1.creditosQuePoseeAlumno();
+									int creditos2 = a2.creditosQuePoseeAlumno();
+									return creditos2 - creditos1;
+								}
+							}
+			);
+			
+		}catch(Exception ex) {
+			log.registrar(this, "imprimir listado",this.inscriptos.size()+ " registros ");
+		}finally {
+				System.out.println(inscriptos);		
+		}
+		
 	}
+	
 
 
 }
